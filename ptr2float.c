7b6abcd01674de3d03105e5e51e9434aa9bad20a
Pascal Cuoq <pascal.cuoq@trust-in-soft.com>
2021-04-15 18:39:49 +0200

    ptr2float

---
+++ ptr2float.c
@@ -0,0 +1,22 @@
+
+float ptr2float(const char* p) {
+  _Static_assert(sizeof (float) == 4);
+  union { char repr[4]; float f; } u = {0,0,0,64};
+
+  if(2.0f == u.f) {
+    for(int i=0;i<sizeof u;i++) u.repr[i]=p[i];
+  }else{
+    for(int i=0;i<sizeof u;i++) u.repr[sizeof(r)-i-1]=p[i];
+  }
+
+  return u.f;
+}
+
+int main(void) {
+  char i1[4] = {0,0,0,64};
+  char i2[4] = {64,0,0,0};
+  char i3[4] = {0,0,0,0};
+  printf("%f\n", ptr2float(i1));
+  printf("%f\n", ptr2float(i2));
+  printf("%f\n", ptr2float(i3));
+}
